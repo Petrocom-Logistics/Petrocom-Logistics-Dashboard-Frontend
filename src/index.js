a8d11f86f6a60884b3b6a770b369dbfdc611ca3d
Rajiv Ranjan <rajivforyu@gmail.com>
2024-01-23 19:57:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
-axios.defaults.baseURL = "https://petrocom.techiesgateway.com";
+axios.defaults.baseURL = "https://dashboard-backend.petrocomlogistics.co.uk";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
